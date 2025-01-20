083d0cfca8954a51087fa99818144c85f7c1f4e3
Roxy <anamarca.sistemas@gmail.com>
2025-01-20 15:58:40 -0400

    Generando el archivo jar

--- src/main/java/foro/hub/api/controller/UsuarioController.java
+++ src/main/java/foro/hub/api/controller/UsuarioController.java
@@ -2,7 +2,6 @@ package foro.hub.api.controller;
 
 import foro.hub.api.domain.usuario.*;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.transaction.Transactional;
